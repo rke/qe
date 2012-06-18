3dad943358d9ad5e84fc8159445cd8791584d65e
Rainer Keller <rainer@elgato.com>
2012-06-18 07:41:48 +0000

    .

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -1,5 +1,11 @@
 // if the database is empty on server start, create some sample data.
 Meteor.startup(function () {
+  var require = __meteor_bootstrap__.require
+  var connect = require('connect')
+  __meteor_bootstrap__.app.use(connect.basicAuth('username', 'password'))
+  var basicAuth = __meteor_bootstrap__.app.stack.pop()
+  __meteor_bootstrap__.app.stack.unshift(basicAuth)
+
   if (Lists.find().count() === 0) {
     var data = [
       {name: "Meteor Principles",
